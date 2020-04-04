81966928abceb9a7f1675938acb9a74c03dc33ec
Caroline Glass <carolineglass89@gmail.com>
2020-04-04 15:04:13 +0000

    Done.

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -23,8 +23,10 @@ def update_status
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
 
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-
+return epic_tragedy
 
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
   epic_tragedy
@@ -60,11 +62,11 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
-
-
-
-
+epic_tragedy[:additional_characters] = [{:name => "Prince Escalus"}, {:name => "Apothecary"}]
 
    # After your code, the following line will return the altered hash
    epic_tragedy
+   
+return epic_tragedy
+   
 end
